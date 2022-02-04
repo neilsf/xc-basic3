f6964e3be8260b6edbff0cb95aed6a764ef61e22
Csaba Fekete <feketecsaba@gmail.com>
2022-02-04 10:29:57 +0100

    Version shift

--- source/app.d
+++ source/app.d
@@ -18,7 +18,7 @@ import compiler.compiler, compiler.library, compiler.sourcefile;
 import globals, optimizer;
 
 // Program version
-const string APP_VERSION = "v3.0.0-beta1";
+const string APP_VERSION = "v3.0.0-beta2";
 
 /** Possible target options */
 const string[] targetOpts = [

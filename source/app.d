6e2824980a320b638e7664f584e1b95bf0e68365
Csaba Fekete <feketecsaba@gmail.com>
2022-03-13 08:19:53 +0100

    Finalizing v3 release (1)

--- source/app.d
+++ source/app.d
@@ -18,7 +18,7 @@ import compiler.compiler, compiler.library, compiler.sourcefile;
 import globals, optimizer;
 
 // Program version
-const string APP_VERSION = "v3.0.0-beta3";
+const string APP_VERSION = "v3.0.0";
 
 /** Possible target options */
 const string[] targetOpts = [

c768ec41534669ae3cb3a622c5b7033fdb699093
Csaba Fekete <feketecsaba@gmail.com>
2022-04-15 11:43:41 +0200

    Fixed erroneous WORD comparisons

--- lib/core/comp/_word.asm
+++ lib/core/comp/_word.asm
@@ -51,102 +51,81 @@
 	ENDM
 	
 	; Compare two words on stack for less than
-	; older < newer
-	MAC cmpwordlt ; @pull @push
-	IF !FPULL
-	pla
-	sta R0 + 1
-	pla
-	sta R0
-	ELSE
-	sta R0
-	sty R0 + 1
-	ENDIF
-	pla
-	cmp R0 + 1
-	pla
-	sbc R0
-	bcs .false
-	ptrue
-	bne .q
-.false:
-	pfalse
-.q
+	MAC cmpwordlt
+	tsx
+	lda.wx stack + 4
+	cmp.wx stack + 2
+	lda.wx stack + 3
+	sbc.wx stack + 1
+	bcc .true
+	lda #0
+	beq .1
+.true
+	lda #255
+.1
+	sta.wx stack + 4
+	inx
+	inx
+	inx
+	txs
 	ENDM
 	
 	; Compare two words on stack for greater than or equal
-	; older >= newer
-	MAC cmpwordgte ; @pull @push
-	IF !FPULL
-	pla
-	sta R0 + 1
-	pla
-	sta R0
-	ELSE
-	sta R0
-	sty R0 + 1
-	ENDIF
-	pla
-	cmp R0 + 1
-	pla
-	sbc R0
+	MAC cmpwordgte
+	tsx
+	lda.wx stack + 4
+	cmp.wx stack + 2
+	lda.wx stack + 3
+	sbc.wx stack + 1
 	bcs .true
-	pfalse
-	beq .q
-.true:
-	ptrue
-.q
+	lda #0
+	beq .1
+.true
+	lda #255
+.1
+	sta.wx stack + 4
+	inx
+	inx
+	inx
+	txs
 	ENDM
 	
 	; Compare two words on stack for greater than
-	; older > newer
-	MAC cmpwordgt ; @push
+	MAC cmpwordgt
 	tsx
-	lda.wx stack+2
-	cmp.wx stack+4
-	lda.wx stack+1
-	sbc.wx stack+3
-	bcc .true	
-	inx
+	lda.wx stack + 2
+	cmp.wx stack + 4
+	lda.wx stack + 1
+	sbc.wx stack + 3
+	bcc .true
+	lda #0
+	beq .1
+.true
+	lda #255
+.1
+	sta.wx stack + 4
 	inx
 	inx
 	inx
 	txs
-	pfalse
-	beq .q
-.true:
-	inx
-	inx
-	inx
-	inx
-	txs
-	ptrue
-.q
 	ENDM
 	
 	; Compare two words on stack for less than or equal
-	; older <= newer
-	MAC cmpwordlte ; @pull @push
-	IF !FPULL
-	pla
-	sta R0 + 1
-	pla
-	sta R0
-	ELSE
-	sta R0
-	sty R0 + 1
-	ENDIF
-	pla
-	cmp R0 + 1
-	bne .1
-	pla
-	cmp R0
-.1  bcc .true  ; lower
-	bne .false ; higher
+	MAC cmpwordlte
+	tsx
+	lda.wx stack + 2
+	cmp.wx stack + 4
+	lda.wx stack + 1
+	sbc.wx stack + 3
+	bcs .true
+	lda #0
+	beq .1
 .true
-	ptrue
-	bne .q
-.false:
-	pfalse
-.q
+	lda #255
+.1
+	sta.wx stack + 4
+	inx
+	inx
+	inx
+	txs
 	ENDM

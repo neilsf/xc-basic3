e5230a06f1e2097dde0d4c5a984dc6e4c4f12e7f
Csaba Fekete <csaba.fekete@instructure.com>
2022-05-26 13:23:04 +0200

    Added implicit STRING variable definition

--- source/statement/let_stmt.d
+++ source/statement/let_stmt.d
@@ -4,7 +4,7 @@ import std.array, std.conv;
 
 import pegged.grammar;
 
-import compiler.compiler, compiler.variable;
+import compiler.compiler, compiler.variable, compiler.type;
 import language.statement, language.expression;
 
 /** Compiles a LET statement */
@@ -23,6 +23,7 @@ class Let_stmt : Statement
         ParseTree expNode = this.node.children[0].children[1];
 
         Expression exp = new Expression(expNode, compiler);
+        bool evaluated = false;
         
         VariableAccess access = new VariableAccess(varNode, compiler, false);
         if(access.getVariable() is null) {
@@ -30,23 +31,35 @@ class Let_stmt : Statement
             if(varNode.children.length > 1) {
                 compiler.displayError("Cannot implicitly define variable with subscript and/or field access");
             }
-            if(!exp.getType().isNumeric()) {
-                compiler.displayError("Only variables of numeric types can be implicitly defined. Use DIM to define \"" 
+            if(exp.getType().name == Type.STRING && !exp.isConstantString()) {
+                compiler.displayError("Cannot implicitly define variable of type STRING unless the right-hand side is a constant");
+            } else if(!exp.getType().isPrimitive) {
+                compiler.displayError("Only variables of primitive types can be implicitly defined. Use DIM to define \"" 
                     ~ join(varNode.matches) ~ "\"");
             }
             Variable var = Variable.create(join(varNode.matches), exp.getType(), compiler);
+            ulong strLen = 0;
+            if(exp.getType().name == Type.STRING) {
+                exp.eval();
+                evaluated = true;
+                strLen = exp.getConstantStringLength();
+                var.strLen = to!ushort(strLen);
+            }
             compiler.getVars().add(var, false);
             access.setVariable(var);
-            compiler.displayNotice("Variable \"" ~ var.name ~ "\" implicitly defined as " ~ var.type.name);
+            compiler.displayNotice("Variable \"" ~ var.name ~ "\" implicitly defined as " ~ var.type.name
+                ~ (strLen > 0 ? (" * " ~ to!string(strLen)) : ""));
         }
 
         if(access.getVariable().isConst) {
             compiler.displayError("Constants can not change value");
         }
         
-        exp.setExpectedType(access.getType());
-        exp.eval();
-        this.appendCode(to!string(exp));
+        if(!evaluated) {
+            exp.setExpectedType(access.getType());
+            exp.eval();
+        }
+        this.appendCode(exp.toString());
         this.appendCode(access.getPullCode());
     }
 }

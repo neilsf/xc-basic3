abd751df00b813804292983f8bcc7fcc3f0ed308
Csaba Fekete <feketecsaba@gmail.com>
2022-07-16 08:52:40 +0200

    VIC-20 Iterrupts improved and fixed

--- lib/irq/_vic20.asm
+++ lib/irq/_vic20.asm
@@ -1,32 +1,58 @@
 	IF USEIRQ == 1
+    
 IRQVECTOR EQU $0314
-VIA_AUX	  EQU $911B
-VIA_IRQFL EQU $911D
-VIA_IRQEN EQU $911E
+VIA_TIMERBLO EQU $9128
+VIA_TIMERBHI EQU $9129
+VIA_IRQFL EQU $912D
+VIA_IRQEN EQU $912E
 	; Flags
 	; bit 0 - custom timer interrupts enabled
 	; bit 1 - system interrupts enabled
-IRQFLAGS HEX 02
 IRQ_TIMER EQU 1
 IRQ_SYSTEM EQU 2
+    ; Store timeout value here
+IRQ_TIMER_LATCH HEX 00 00
 
 	MAC irqenable
-	lda #{1}
-	ora IRQFLAGS
-	sta IRQFLAGS
-	ENDM
+    sei
+    IF {1} == IRQ_TIMER
+    lda #%10100000
+    sta VIA_IRQEN
+    lda IRQ_TIMER_LATCH
+    sta VIA_TIMERBLO
+    lda IRQ_TIMER_LATCH + 1
+    sta VIA_TIMERBHI
+    ENDIF
+    IF {1} == IRQ_SYSTEM
+    lda #%11000000
+    sta VIA_IRQEN
+    ENDIF
+	cli
+    ENDM
 	
 	MAC irqdisable
-	lda #[{1} ^ $FF]
-	and IRQFLAGS
-	sta IRQFLAGS
-	ENDM
+	sei
+    IF {1} == IRQ_TIMER
+    lda #%00100000
+    ENDIF
+    IF {1} == IRQ_SYSTEM
+    lda #%01000000
+    ENDIF
+    sta VIA_IRQEN
+	cli
+    ENDM
 	
 	MAC onirqgosub
 	lda #<{2}
-	sta {1}_V + 1
+	sta IRQ_TIMER_V + 1
 	lda #>{2}
-	sta {1}_V + 2
+	sta IRQ_TIMER_V + 2
+    IF {1} == IRQ_TIMER
+    pla
+    sta IRQ_TIMER_LATCH + 1
+    pla 
+    sta IRQ_TIMER_LATCH
+	ENDIF
 	ENDM
 	
 IRQSETUP SUBROUTINE
@@ -44,24 +70,29 @@ IRQRESET SUBROUTINE
 	sta IRQVECTOR
 	lda #>$EABF
 	sta IRQVECTOR + 1
-	cli
+	lda #%00100000
+    sta VIA_IRQEN
+    lda #%11000000
+    sta VIA_IRQEN
+    cli
 	rts
 	
 XCBIRQ	SUBROUTINE
 	cld
-	lda #IRQ_TIMER
-	bit IRQFLAGS
-	beq .1
+    lda VIA_IRQFL
+    and #%00100000
+    beq .sysirq
 	phsr
 IRQ_TIMER_V
 	jsr $FFFF ; To be modified by application
 	plsr
-.1
-	lda #IRQ_SYSTEM
-	bit IRQFLAGS
-	beq .q
-	jmp $EABF
-.q
-	jmp $EB12
+    lda VIA_TIMERBLO
+    lda IRQ_TIMER_LATCH
+    sta VIA_TIMERBLO
+    lda IRQ_TIMER_LATCH + 1
+    sta VIA_TIMERBHI
+    jmp $EB18
+.sysirq	
+    jmp $EABF
 	
-	ENDIF
+    ENDIF

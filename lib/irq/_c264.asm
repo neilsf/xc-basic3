0efd052ca1fa312a2df7d3c94e7959b4517150f5
Csaba Fekete <feketecsaba@gmail.com>
2022-08-14 14:40:58 +0200

    IRQ fixed on Plus/4

--- lib/irq/_c264.asm
+++ lib/irq/_c264.asm
@@ -1,3 +1,25 @@
+;$FF09: Interrupt request register. When a counter sends want to send an IRQ, it's bit will appear as a 0; then, if the IRQ was caused then highmost bit is set.
+;Bit 0 : Unused
+;Bit 1 : Raster-counter
+;Bit 2 : Lightpen. Not implemented.
+;Bit 3 : Counter #1
+;Bit 4 : Counter #2
+;Bit 5 : Unused
+;Bit 6 : Counter #3
+;Bit 7 : Interrupt occured. This bit is set when an IRQ was enabled and therefore, the IRQ was sent to the processor. Physically, this is the negated level of the TED's IRQ output. The IRQ should be deleted with writing the register-value back after accepting an interrupt.;
+;
+;
+;$FF0A: Interrupt mask register. These bits could be used to disable and enable interrupt-sources. When a place is set to 1, that will
+;be able to cause an interrupt to the processor. If not, the sign of the interrupt request will only be appear in the above register.
+;Bit 0 : 9th bit of $FF0B (see there)
+;Bit 1 : Raster-counter
+;Bit 2 : Lightpen. Not implemented.
+;Bit 3 : Counter #1
+;Bit 4 : Counter #2
+;Bit 5 : Unused
+;Bit 6 : Counter #3
+;Bit 7 : Unused
+
     	IF USEIRQ == 1
 
 IRQVECTOR EQU $0314
@@ -90,16 +112,17 @@ IRQSETUP SUBROUTINE
     sta TED_TIMER1
     lda #$40
     sta TED_TIMER1 + 1
-	lda %01111111
+    lda TED_IRQST
     sta TED_IRQST
     cli
 	rts
     
 XCBIRQ SUBROUTINE
     cld
-    lda #IRQ_RASTER
+    lda TED_IRQMASK
+    and #IRQ_RASTER
     bit TED_IRQST
-    bne .timer
+    beq .timer
     ; Raster interrupt
     phsr
 IRQ_RASTER_V
@@ -108,26 +131,32 @@ IRQ_RASTER_V
     lda #IRQ_RASTER
     jmp .q
 .timer   
-    lda #IRQ_TIMER
+    lda TED_IRQMASK
+    and #IRQ_TIMER
     bit TED_IRQST
-    bne .system
+    beq .system
     ; Timer interrupt
     phsr
 IRQ_TIMER_V
 	jsr $FFFF ; To be modified by application
 	plsr
+    lda IRQ_TIMER_LATCH
+    sta TED_TIMER2
+    lda IRQ_TIMER_LATCH + 1
+    sta TED_TIMER2 + 1
     lda #IRQ_TIMER
     jmp .q
 .system:
-    lda #IRQ_SYSTEM
+    lda TED_IRQMASK
+    and #IRQ_SYSTEM
     bit TED_IRQST
-    bne .q
+    beq .q
     jsr $DB11 ; Read keyboard
     jsr $CEF0 ; Update Jiffy and read Stop key  
     lda #IRQ_SYSTEM
 .q
+    ora TED_IRQST
     sta TED_IRQST
     jmp $FCBE
     
     ENDIF
->>>>>>> release/v3.1

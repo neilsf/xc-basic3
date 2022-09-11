cab85e673c24208035887fbd2ef0f6055b1b5de8
Csaba Fekete <feketecsaba@gmail.com>
2022-09-11 16:14:16 +0200

    Fixes #178, Fixes #168 multiple constants in inline assembly

--- source/statement/rem_stmt.d
+++ source/statement/rem_stmt.d
@@ -1,6 +1,6 @@
 module statement.rem_stmt;
 
-import std.regex;
+import std.regex, std.conv;
 import pegged.grammar;
 import language.statement, compiler.compiler, compiler.variable;
 
@@ -16,16 +16,19 @@ class Rem_stmt : Statement
     {
         if(compiler.inlineAssembly) {
             if(node.children[0].matches.length > 1) {
+                string replaceVariable(Captures!(string) m) {
+                    Variable v = compiler.getVars().findVisible(m.hit[1..$-1]);
+                    if(!(v is null)) {
+                        if(v.isConst) {
+                            return to!string(v.constVal);
+                        }
+                        return v.getAsmLabel();
+                    }
+                    return m.hit;
+                }
                 string line = node.children[0].matches[1][1..$];
                 auto r = regex(r"\{[a-zA-Z_0-9]+\}");
-                auto match = matchFirst(line, r);
-                if(match) {
-                    string varName = match[0][1..$-1];
-                    Variable v = compiler.getVars().findVisible(varName);
-                    if(!(v is null)) {
-                        line = replaceFirst(line, r, v.getAsmLabel());
-                    }
-                }
+                line = replaceAll!(replaceVariable)(line, r);
                 appendCode(line ~ "\n");
             }
         }

a10117ffdac731499d74806327d7035be20da204
Csaba Fekete <feketecsaba@gmail.com>
2022-09-17 10:27:17 +0200

    Backport of bugfix commits from v3.1

--- source/app.d
+++ source/app.d
@@ -18,7 +18,7 @@ import compiler.compiler, compiler.library, compiler.sourcefile;
 import globals, optimizer;
 
 // Program version
-const string APP_VERSION = "v3.0.8";
+const string APP_VERSION = "v3.0.9";
 
 /** Possible target options */
 const string[] targetOpts = [

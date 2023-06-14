1ae98caec9031249f23e2f2e1197abcf1e29cd81
Csaba Fekete <feketecsaba@gmail.com>
2023-06-14 07:17:54 +0200

    Updated version number

--- source/app.d
+++ source/app.d
@@ -18,7 +18,7 @@ import compiler.compiler, compiler.library, compiler.sourcefile;
 import globals, optimizer;
 
 // Program version
-const string APP_VERSION = "v3.1.3";
+const string APP_VERSION = "v3.1.4";
 
 /** Possible target options */
 const string[] targetOpts = [

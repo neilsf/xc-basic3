ec2265a60e555e1ea9f2d65d56d3dcdc1ca2a0fa
Csaba Fekete <feketecsaba@gmail.com>
2023-07-25 06:31:00 +0200

    Adjusted program version

--- source/app.d
+++ source/app.d
@@ -18,7 +18,7 @@ import compiler.compiler, compiler.library, compiler.sourcefile;
 import globals, optimizer;
 
 // Program version
-const string APP_VERSION = "v3.1.6";
+const string APP_VERSION = "v3.1.7";
 
 /** Possible target options */
 const string[] targetOpts = [

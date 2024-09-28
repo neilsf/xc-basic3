100e4f092eb5a159ada665a1538cef3c0d7f6fa2
Csaba Fekete <feketecsaba@gmail.ccom>
2024-09-28 15:53:40 +0200

    Set next version number

--- source/app.d
+++ source/app.d
@@ -18,7 +18,7 @@ import compiler.compiler, compiler.library, compiler.sourcefile;
 import globals, optimizer;
 
 // Program version
-const string APP_VERSION = "v3.2.1";
+const string APP_VERSION = "v3.2.0";
 
 /** Possible target options */
 const string[] targetOpts = [

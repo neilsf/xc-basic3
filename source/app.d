3c1092e4452618ff96a854f16d34531d6a094252
JJFlash-IT <jjflash@ik.me>
2026-08-21 09:43:20 +0200

    Remove the "-JJ" suffix (sorry...)

--- source/app.d
+++ source/app.d
@@ -18,7 +18,7 @@ import compiler.compiler, compiler.library, compiler.sourcefile;
 import globals, optimizer;
 
 // Program version
-const string APP_VERSION = "v3.1.13-JJ";
+const string APP_VERSION = "v3.1.13";
 
 /** Possible target options */
 const string[] targetOpts = [
